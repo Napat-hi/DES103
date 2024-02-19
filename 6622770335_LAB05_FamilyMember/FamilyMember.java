ea5dceeaf6d4ff86b54918d421070bff13178bea
Napat-hi <napat6603@gmail.com>
2024-02-19 21:49:48 +0700

    first commit

---
+++ 6622770335_LAB05_FamilyMember/FamilyMember.java
@@ -0,0 +1,29 @@
+public class FamilyMember{
+
+   static String familyName = "Hilton";
+    String firstname;
+    static double commonFund = 100000.00;
+    double privateFund;
+
+    public FamilyMember(String familyName, double privateFund) {
+        this.firstname = familyName;
+        this.privateFund = privateFund;
+    }
+
+    void printPrivateFund(){
+        System.out.println(firstname + " " + familyName + "has $" + privateFund);
+    }
+
+    void contributeToCommonFund(double amount){
+        commonFund += amount;
+        privateFund -= amount;
+    }
+
+    static void payFromCommonFund(double amount){
+        commonFund -= amount;
+    }
+
+    static void printFamilyFund(){
+        System.out.println("The " + familyName + "family has $" + commonFund);
+    }
+}
